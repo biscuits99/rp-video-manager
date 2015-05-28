c5a62929375c1273a937ee33abcf2df93a99a735
Floob <biscuits99@users.noreply.github.com>
2015-05-28 22:44:20 +0100

    Path change rp

--- videomode.sh
+++ videomode.sh
@@ -10,72 +10,72 @@ do
 
         "Default")
 # SNES
-cp ~/video-manager/system-retroarch/snes/retroarch-original.cfg /opt/retropie/configs/snes/retroarch.cfg
-cp ~/video-manager/all-videomodes/videomodes-original.cfg /opt/retropie/configs/all/videomodes.cfg
+cp ~/rp-video-manager/system-retroarch/snes/retroarch-original.cfg /opt/retropie/configs/snes/retroarch.cfg
+cp ~/rp-video-manager/all-videomodes/videomodes-original.cfg /opt/retropie/configs/all/videomodes.cfg
             echo "Settings are back to default"
 break
             ;;
 
         "Shader")
 echo "Updating files to use snes.glslp shader"
-cp ~/video-manager/system-retroarch/retroarch-shader.cfg /opt/retropie/configs/snes/retroarch.cfg
-cp ~/video-manager/all-videomodes/videomodes-original.cfg /opt/retropie/configs/all/videomodes.cfg
+cp ~/rp-video-manager/system-retroarch/retroarch-shader.cfg /opt/retropie/configs/snes/retroarch.cfg
+cp ~/rp-video-manager/all-videomodes/videomodes-original.cfg /opt/retropie/configs/all/videomodes.cfg
             echo "Shader has now been applied"
             break
             ;;
 
         "Overlays")
 # Copy Shader
-cp ~/video-manager/system-retroarch/shaders/Brighter-with_Contrast.glslp /opt/retropie/emulators/retroarch/shader/
+cp ~/rp-video-manager/system-retroarch/shaders/Brighter-with_Contrast.glslp /opt/retropie/emulators/retroarch/shader/
 
 # Copy VideoModes
-cp ~/video-manager/all-videomodes/videomodes-updated.cfg /opt/retropie/configs/all/videomodes.cfg
+cp ~/rp-video-manager/all-videomodes/videomodes-updated.cfg /opt/retropie/configs/all/videomodes.cfg
 
 # SNES
-cp ~/video-manager/system-retroarch/snes/retroarch-overlay.cfg /opt/retropie/configs/snes/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/snes/retroarch-overlay.cfg /opt/retropie/configs/snes/retroarch.cfg
 
 # Atari Lynx
-cp ~/video-manager/system-retroarch/atarilynx/retroarch-overlay.cfg /opt/retropie/configs/atarilynx/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/atarilynx/retroarch-overlay.cfg /opt/retropie/configs/atarilynx/retroarch.cfg
 
 # FBA
-cp ~/video-manager/system-retroarch/fba/retroarch-overlay.cfg /opt/retropie/configs/fba/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/fba/retroarch-overlay.cfg /opt/retropie/configs/fba/retroarch.cfg
 
 # GameGear
-cp ~/video-manager/system-retroarch/gamegear/retroarch-overlay.cfg /opt/retropie/configs/gamegear/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/gamegear/retroarch-overlay.cfg /opt/retropie/configs/gamegear/retroarch.cfg
 
 # Game Boy
-cp ~/video-manager/system-retroarch/gb/retroarch-overlay.cfg /opt/retropie/configs/gb/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/gb/retroarch-overlay.cfg /opt/retropie/configs/gb/retroarch.cfg
 
 # Game Boy Advance
-cp ~/video-manager/system-retroarch/gba/retroarch-overlay.cfg /opt/retropie/configs/gba/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/gba/retroarch-overlay.cfg /opt/retropie/configs/gba/retroarch.cfg
 
 # Game Boy Colour
-cp ~/video-manager/system-retroarch/gbc/retroarch-overlay.cfg /opt/retropie/configs/gbc/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/gbc/retroarch-overlay.cfg /opt/retropie/configs/gbc/retroarch.cfg
 
 # Sega MasterSystem
-cp ~/video-manager/system-retroarch/mastersystem/retroarch-overlay.cfg /opt/retropie/configs/mastersystem/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/mastersystem/retroarch-overlay.cfg /opt/retropie/configs/mastersystem/retroarch.cfg
 
 # Sega Megadrive
-cp ~/video-manager/system-retroarch/megadrive/retroarch-overlay.cfg /opt/retropie/configs/megadrive/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/megadrive/retroarch-overlay.cfg /opt/retropie/configs/megadrive/retroarch.cfg
 
 # Neo Geo
-cp ~/video-manager/system-retroarch/neogeo/retroarch-overlay.cfg /opt/retropie/configs/neogeo/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/neogeo/retroarch-overlay.cfg /opt/retropie/configs/neogeo/retroarch.cfg
 
 # NES
-cp ~/video-manager/system-retroarch/nes/retroarch-overlay.cfg /opt/retropie/configs/nes/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/nes/retroarch-overlay.cfg /opt/retropie/configs/nes/retroarch.cfg
 
 # PSX
-cp ~/video-manager/system-retroarch/psx/retroarch-overlay.cfg /opt/retropie/configs/psx/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/psx/retroarch-overlay.cfg /opt/retropie/configs/psx/retroarch.cfg
 
 # Sega 32x
-cp ~/video-manager/system-retroarch/sega32x/retroarch-overlay.cfg /opt/retropie/configs/sega32x/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/sega32x/retroarch-overlay.cfg /opt/retropie/configs/sega32x/retroarch.cfg
 
 # Sega CD
-cp ~/video-manager/system-retroarch/segacd/retroarch-overlay.cfg /opt/retropie/configs/segacd/retroarch.cfg
+cp ~/rp-video-manager/system-retroarch/segacd/retroarch-overlay.cfg /opt/retropie/configs/segacd/retroarch.cfg
 
 # General overlays
 mkdir -p /opt/retropie/emulators/retroarch/overlays/16-9
-cp -r ~/video-manager/16-9/* /opt/retropie/emulators/retroarch/overlays/16-9/
+cp -r ~/rp-video-manager/16-9/* /opt/retropie/emulators/retroarch/overlays/16-9/
             echo "Overlays have now been applied"
             break
             ;;
